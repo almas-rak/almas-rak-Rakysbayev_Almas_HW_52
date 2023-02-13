551aab964f83c4b1f66525668674abd0d8418282
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-14 01:49:41 +0600

    добавление модели списка задач в админку

--- source/to_do_list_app/admin.py
+++ source/to_do_list_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from to_do_list_app.models import Todo
+
+
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['description', 'status', 'date_of_completion']
+    list_filter = ['description', 'status', 'date_of_completion']
+
+
+admin.site.register(Todo, TodoAdmin)
