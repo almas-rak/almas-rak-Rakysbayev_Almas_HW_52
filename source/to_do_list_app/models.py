7945c70a2817ac1a2ca39096abb845b726f8d6a3
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-14 01:11:11 +0600

    создание модели задач

--- source/to_do_list_app/models.py
+++ source/to_do_list_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('completed', 'Завершено')
+    ]
+
+    description = models.CharField(max_length=200, null=False, verbose_name='Описание')
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
