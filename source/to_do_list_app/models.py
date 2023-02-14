1e2cf0b42149789fbfb073a574a89591b6ee2220
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-14 15:29:09 +0600

    добавление заголовка в модель

--- source/to_do_list_app/models.py
+++ source/to_do_list_app/models.py
@@ -9,9 +9,10 @@ class Todo(models.Model):
         ('completed', 'Завершено')
     ]
 
-    description = models.CharField(max_length=200, null=False, verbose_name='Описание')
+    title = models.CharField(max_length=200, null=False, verbose_name='Заголовок')
+    description = models.CharField(max_length=200, null=True, verbose_name='Описание')
     status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
-    date_of_completion = models.DateField(blank=True, null=True)
+    date_of_completion = models.DateField(blank=True, null=True, verbose_name='Дата завершения')
 
     def __str__(self):
         return f'{self.description} - {self.status}'
