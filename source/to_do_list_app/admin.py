4e34c2fd3912af4f59c22ea851d23d4b7772241d
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-14 18:14:32 +0600

    в админку добавил поиск и изменение моделей задач

--- source/to_do_list_app/admin.py
+++ source/to_do_list_app/admin.py
@@ -5,8 +5,10 @@ from to_do_list_app.models import Todo
 
 # Register your models here.
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ['description', 'status', 'date_of_completion']
-    list_filter = ['description', 'status', 'date_of_completion']
+    list_display = ['id', 'title', 'description', 'status', 'date_of_completion']
+    list_filter = ['title', 'description', 'status', 'date_of_completion']
+    list_editable = ['title', 'status', 'date_of_completion']
+    search_fields = ['title', 'description', 'date_of_completion']
 
 
 admin.site.register(Todo, TodoAdmin)
